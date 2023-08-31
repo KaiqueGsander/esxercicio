2efa1ab8837c37c07de7b044f578a93657a0f681
kaique sander <125375896+KaiqueGsander@users.noreply.github.com>
2023-08-31 18:28:40 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function exercicio1() {
+  var nota = parseInt(prompt("digite um numero"));
+  while (nota < 0 || nota > 10) {
+    nota = parseInt(prompt("digite novamente a nota"));
+  }
+  document.getElementById("root").style.display = "block"
+  document.getElementById("root").innerHTML = `<p> sua mae é: ${nota}</p>`
+  }
+function exercicio2(duration, display){
+var timer = duration seconds
+
+}
